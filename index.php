16bb2a9f3c6f2331856f55623a1da969f461e0d2
Nikola <nikolkady@gmail.com>
2019-12-08 18:07:51 +0400

    Add crud for generates lead

--- index.php
+++ index.php
@@ -1,44 +1,81 @@
-<?
-/**
- * Write data to log file.
- *
- * @param mixed $data
- * @param string $title
- *
- * @return bool
- */
-function writeToLog($data, $title = '') {
-    $log = "\n------------------------\n";
-    $log .= date("Y.m.d G:i:s") . "\n";
-    $log .= (strlen($title) > 0 ? $title : 'DEBUG') . "\n";
-    $log .= print_r($data, 1);
-    $log .= "\n------------------------\n";
-    file_put_contents(getcwd() . '/hook.log', $log, FILE_APPEND);
-    return true;
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+if(isset($_REQUEST['delete'])){
+
+    $deleted = delete_lead($_REQUEST['delete']);
+
 }
 
-$defaults = array('first_name' => '', 'last_name' => '', 'phone' => '', 'email' => '');
+if(isset($_REQUEST['firstname']) && isset($_REQUEST['lastname'])) {
+    print_r($_REQUEST);exit();
+    create_lead($_REQUEST);
+}
 
-if (array_key_exists('saved', $_REQUEST)) {
-    $defaults = $_REQUEST;
-    writeToLog($_REQUEST, 'webform');
 
-    $queryUrl = 'https://restapi.bitrix24.ru/rest/1/wlrsr314o80qg8wf/crm.lead.add.json';
-    $queryData = http_build_query(array(
+
+
+
+function delete_lead($id = null){
+
+    $queryUrl = 'https://b24-d2ybw4.bitrix24.ru/rest/1/wlrsr314o80qg8wf/crm.lead.delete';
+    $queryData = http_build_query([
+        'id' => $id
+    ]);
+
+
+    return curl_send_request($queryUrl , $queryData);
+
+}
+
+function create_lead($fields = null){
+
+    $queryUrl = 'https://b24-d2ybw4.bitrix24.ru/rest/1/wlrsr314o80qg8wf/crm.lead.add';
+    $queryData = http_build_query([
         'fields' => array(
-            "TITLE" => $_REQUEST['first_name'].' '.$_REQUEST['last_name'],
-            "NAME" => $_REQUEST['first_name'],
-            "LAST_NAME" => $_REQUEST['last_name'],
+            "TITLE" => 'Заявка от ' . $fields['firstname']. ' '. $fields['lastname'],
+            "NAME" => $fields['firstname'],
+            "LAST_NAME" => $fields['lastname'],
             "STATUS_ID" => "NEW",
             "OPENED" => "Y",
             "ASSIGNED_BY_ID" => 1,
-            "PHONE" => array(array("VALUE" => $_REQUEST['phone'], "VALUE_TYPE" => "WORK" )),
-            "EMAIL" => array(array("VALUE" => $_REQUEST['email'], "VALUE_TYPE" => "WORK" )),
+            "CURRENCY_ID"=> "USD",
+            "OPPORTUNITY" => 12500,
+            "PHONE" => array(array("VALUE" => $fields['phone'], "VALUE_TYPE" => "WORK" )),
+            "EMAIL" => array(array("VALUE" => $fields['email'], "VALUE_TYPE" => "WORK" )),
         ),
         'params' => array("REGISTER_SONET_EVENT" => "Y")
-    ));
+    ]);
+
+    $result = curl_send_request($queryUrl , $queryData);
+
+//echo '<pre>';print_r($result);exit();
+
+//    if (array_key_exists('error', $result)) echo "Ошибка при сохранении лида: ".$result['error_description']."<br/>";
+}
+
+function list_lead(){
+
+    $queryUrl = 'https://b24-d2ybw4.bitrix24.ru/rest/1/wlrsr314o80qg8wf/crm.lead.list';
+    $queryData = http_build_query([
+        'id' => 6
+    ]);
+    return curl_send_request($queryUrl, $queryData);
+}
+
+/**
+ * Send curl query to portal
+ *
+ * @param $queryUrl
+ * @param $queryData
+ * @return array
+ */
+function curl_send_request($queryUrl , $queryData){
+
 
     $curl = curl_init();
+
     curl_setopt_array($curl, array(
         CURLOPT_SSL_VERIFYPEER => 0,
         CURLOPT_POST => 1,
@@ -47,22 +84,15 @@ if (array_key_exists('saved', $_REQUEST)) {
         CURLOPT_URL => $queryUrl,
         CURLOPT_POSTFIELDS => $queryData,
     ));
-
     $result = curl_exec($curl);
     curl_close($curl);
 
-    $result = json_decode($result, 1);
-    writeToLog($result, 'webform result');
+    return json_decode($result, 1);
 
-    if (array_key_exists('error', $result)) echo "Ошибка при сохранении лида: ".$result['error_description']."<br/>";
 }
 
-?>
-<form method="post" action="index.php">
-    Имя: <input type="text" name="first_name" size="15" value="<?=$defaults['first_name']?>"><br/>
-    Фамилия: <input type="text" name="last_name" size="15" value="<?=$defaults['last_name']?>"><br/>
-    Телефон: <input type="phone" name="phone" value="<?=$defaults['phone']?>"><br/>
-    E-mail: <input type="email" name="email" value="<?=$defaults['email']?>"><br/>
-    <input type="hidden" name="saved" value="yes">
-    <input type="submit" value="Отправить">
-</form>
+$list_lead = list_lead();
+
+//echo '<pre>';print_r($list_lead);exit;
+
+include 'index.phtml';
